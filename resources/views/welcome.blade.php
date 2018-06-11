5e1172eb2386147973fd854c96574d980cf977d5
brunodhr <brunofiliperamos@hotmail.com>
2018-06-11 17:45:44 -0300

    Teste Postman

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE HTML>
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('/teste',function (Request $request){
+    return "ok";
+});;
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
